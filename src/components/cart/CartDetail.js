b6f3b910ac55e364d6c33ae9ae0d60544e6d7a67
hakancelikten <celikten93@gmail.com>
2024-09-09 23:31:31 +0300

    cartdetail eklendi

---
+++ src/components/cart/CartDetail.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import * as cartActions from '../../redux/actions/cartActions'
+import { connect } from 'react-redux';
+
+class CartDetail extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+function mapStateToProps(state) {
+    return {
+        cart: state.cartReducer
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: {
+            removeFromCart: bindActionCreators(cartActions.removeFromCart, dispatch),
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartDetail)

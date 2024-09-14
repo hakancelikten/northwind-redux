14db791a9d413ae27a5767857cde2ebdbd15a4fa
hakancelikten <celikten93@gmail.com>
2024-09-14 10:11:48 +0300

    notfound

--- src/components/root/App.js
+++ src/components/root/App.js
@@ -4,6 +4,7 @@ import Dashboard from "./Dashboard";
 import { Route, Routes } from "react-router-dom";
 import CartDetail from "../cart/CartDetail";
 import AddOrUpdateProduct from "../products/AddOrUpdateProduct";
+import NotFound from "../common/NotFound";
 function App() {
   return (
     <Container>
@@ -16,6 +17,8 @@ function App() {
           path="/saveproduct/:productId"
           element={<AddOrUpdateProduct />}
         />
+        <Route path="/saveproduct" element={<AddOrUpdateProduct />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Container>
   );
